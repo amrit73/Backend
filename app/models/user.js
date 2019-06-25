c30fc9cd163f0918046608bcf6937386c3f33f4e
amrit73 <amritxrestha75@gmail.com>
2019-06-25 12:27:35 -0500

    login api

---
+++ app/models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// set up a mongoose model
+module.exports = mongoose.model('User', new Schema({
+    unique_id: Number,
+    name: String,
+    email: String,
+    phone: String,
+    username: String,
+    password: String,
+    passwordConf: String,
+    user_status: String,
+    admin: Boolean,
+}));
